ce9b6abb19319434051d3d38f0e75ed651ae64e4
Gabriel Bastianelle <gsbastianelle@gmail.com>
2023-01-03 12:07:06 +0000

    schemasql and editorconfig

--- images/db/xml/initial_schema.sql
+++ images/db/xml/initial_schema.sql
@@ -3,7 +3,8 @@ CREATE TABLE public.imported_documents (
 	file_name       VARCHAR(250) UNIQUE NOT NULL,
 	xml             XML NOT NULL,
 	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
-	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
+	updated_on      TIMESTAMP NOT NULL DEFAULT NOW(),
+	estado boolean DEFAULT TRUE
 );
 
 CREATE TABLE public.converted_documents (
