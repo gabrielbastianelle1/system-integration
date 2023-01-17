f913d7e5eaf405ded182a940b23138d7307ef30b
Gabriel Bastianelle <gsbastianelle@gmail.com>
2023-01-17 15:23:34 +0000

    api-proc: endpoints done

--- src/api/proc/main.py
+++ src/api/proc/main.py
@@ -1,23 +1,91 @@
 import sys
 
-from flask import Flask
+from flask import Flask, request
+
+import psycopg2
+from psycopg2 import OperationalError
+
+from flask_cors import CORS, cross_origin
+
 
 PORT = int(sys.argv[1]) if len(sys.argv) >= 2 else 9000
 
 app = Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
 
+def connect_to_database():
+    db_xml = psycopg2.connect(host='db-xml', database='is', user='is', password='is')
+    xml_cursor = db_xml.cursor()
+    return db_xml, xml_cursor
 
-@app.route('/api/best_players', methods=['GET'])
-def get_best_players():
-    return [{
-        "id": "7674fe6a-6c8d-47b3-9a1f-18637771e23b",
-        "name": "Ronaldo",
-        "country": "Portugal",
-        "position": "Striker",
-        "imgUrl": "https://cdn-icons-png.flaticon.com/512/805/805401.png",
-        "number": 7
-    }]
+
+@app.route('/api/movies/order', methods=['GET'])
+def order_total_movies_per_country():
+    query = f"select unnest(xpath('.//type/release_year/country/@country', xml))::text as countries, count(*) as total_movies from imported_documents where id = 1 group by countries order by total_movies desc;"
+    db_xml, xml_cursor=connect_to_database()
+    xml_cursor.execute(query)
+    response = xml_cursor.fetchall()
+
+    db_xml.close()
+
+    return response
+
+@app.route('/api/movies/score', methods=['GET'])
+def order_movie_per_score():
+    query = f"select unnest(xpath('(.//type/release_year/country/movie[number(score)>number(6)])/title/text()', xml))::text as title,unnest(xpath('(.//type/release_year/country/movie[number(score)>number(6)])/score/text()', xml))::text::float as score from imported_documents where id = 1 order by score desc;"
+    db_xml, xml_cursor=connect_to_database()
+    xml_cursor.execute(query)
+    response = xml_cursor.fetchall()
+
+    db_xml.close()
+
+    return response
+
+@app.route('/api/movies/data', methods=['GET'])
+def get_all_data_movie():
+
+    title = request.args.get('title')
+
+    query = f'''with movie as (
+                SELECT
+                    unnest(xpath('(.//movie[title =\"{title}\"])', xml)) as m
+                from
+                    imported_documents
+                where
+                    id=1)
+                SELECT
+                    (xpath('//title/text()', m))[1]::text as title,
+                    (xpath('//score/text()', m))[1]::text::float as score,
+                    (xpath('//duration/text()', m))[1]::text as duration,
+                    (xpath('//rating/text()', m))[1]::text as rating,
+                    (xpath('//city/text()', m))[1]::text as city,
+                    (xpath('//listed_in/text()', m))[1]::text as category,
+                    (xpath('//director/text()', m))[1]::text as director
+                FROM movie'''
+
+    db_xml, xml_cursor=connect_to_database()
+    xml_cursor.execute(query)
+    response = xml_cursor.fetchall()
+
+    db_xml.close()
+
+    return response
+
+@app.route('/api/movies/rating', methods=['GET'])
+def count_movies_per_rating():
+
+    year = request.args.get('year')
+
+    query = f"select unnest(xpath('(.//type/release_year[@release_year=number({year})]/country/movie)/rating/text()', xml))::text as rating, count(*) as totalMovies from imported_documents where id = 1 group by rating order by totalmovies desc;"
+
+    db_xml, xml_cursor=connect_to_database()
+    xml_cursor.execute(query)
+    response = xml_cursor.fetchall()
+
+    db_xml.close()
+
+    return response
 
 
 if __name__ == '__main__':
--- src/server/rpc/main.py
+++ src/server/rpc/main.py
@@ -2,9 +2,6 @@ import signal, sys
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 
-from functions.string_length import string_length
-from functions.string_reverse import string_reverse
-
 PORT = int(sys.argv[1]) if len(sys.argv) >= 2 else 9000
 
 if __name__ == "__main__":
@@ -28,8 +25,39 @@ if __name__ == "__main__":
         signal.signal(signal.SIGINT, signal_handler)
 
         # register both functions
-        server.register_function(string_reverse)
-        server.register_function(string_length)
+        def query(self, query):
+            return self.db.query(query)
+
+        def delete_xml_file(self, id) -> None:
+            self.db.delete_xml_file(id)
+
+        def list_all_xml_file(self) -> list:
+            files = self.db.list_all_xml_inserted()
+            return files
+
+        def insert_xml_file(self, file_name, xml_file, xml_path) -> str:
+            """ handlevalidation = Handlevalidation()
+
+            if handlevalidation.validate(xml_path, "movies.xsd"):
+                self.db.insert_xml_to_db(file_name, xml_file)
+                return "xml inserted"
+            else:
+                return "xml is not valid"     """
+            pass
+
+        def convert_csv_to_xml(self, total_lines) -> str:
+            """ Handlexml(total_lines)
+            self.db.create_table_and_insert_initial_xml()
+
+            return "created!" """
+            pass
+
+        server.register_function(convert_csv_to_xml, "convert_csv_to_xml")
+        server.register_function(insert_xml_file, "insert_xml_file")
+        server.register_function(list_all_xml_file, "list_all_xml_file")
+        server.register_function(delete_xml_file, "delete_xml_file")
+        server.register_function(query, "query")
+
 
         # start the server
         print(f"Starting the RPC Server in port {PORT}...")
